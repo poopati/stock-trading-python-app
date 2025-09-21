ea2d2f3b1cc9598dba63c5806b395a2e9aa7e3fc
poopati <keerthipatipoojasree.com>
2025-09-21 16:32:24 -0500

    correct the syntax in scheduler

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ def basic_job():
 # Run every minute
 schedule.every().minute.do(basic_job)
 # Run every minute
-schedule.every().minute().do(run_stock_job)
+schedule.every().minute.do(run_stock_job)
 
 while True:
     schedule.run_pending()
